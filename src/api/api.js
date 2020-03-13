4ad1582bd3ee103437be743bd375767a0b2f82ab
Edgar <ed.galstyan01@gmail.com>
2020-03-13 18:02:00 +0400

    fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,6 @@
 import * as axios from "axios";
 
-const DEFAULT_API_URL = process.env.API_URL || 'https://api.openweathermap.org/data/2.5/weather';
-const WEEK_API_URL = process.env.WEEK_API_URL || 'https://api.openweathermap.org/data/2.5/forecast';
+const API_URL = process.env.API_URL || 'https://api.openweathermap.org/data/2.5';
 const API_KEY = process.env.API_KEY || '156a2c5b9041b784eccada8f35bb12cd';
 const UNITS = 'metric';
 
@@ -16,14 +15,14 @@ const UNITS = 'metric';
 
 export const weatherApi = {
     getWeather(coords) {
-        return axios.get(`${DEFAULT_API_URL}?lat=${coords.lat}&lon=${coords.lon}&appid=${API_KEY}&units=${UNITS}`)
+        return axios.get(`${API_URL}/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${API_KEY}&units=${UNITS}`)
             .then((response) => {
                 return response.data;
             })
             .catch(err => alert(err));
     },
     getweekWeather(coords) {
-        return axios.get(`${WEEK_API_URL}?lat=${coords.lat}&lon=${coords.lon}&appid=${API_KEY}&units=${UNITS}`)
+        return axios.get(`${API_URL}/forecast?lat=${coords.lat}&lon=${coords.lon}&appid=${API_KEY}&units=${UNITS}`)
             .then((response) => {
                 return response.data;
             })
